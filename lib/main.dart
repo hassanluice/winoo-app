a9a058a77c8097ab869d0c093c9a34efa9358e9e
hassanluice <hassanluiceofficial@gmail.com>
2025-04-09 23:19:08 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/admin_panel.dart';
+import 'screens/slot_machine.dart';
+import 'screens/dice_game.dart';
+import 'screens/wheel_spin.dart';
 
 void main() {
-  runApp(WinooApp());
+  runApp(const WinooApp());
 }
 
 class WinooApp extends StatelessWidget {
+  const WinooApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,14 +21,15 @@ class WinooApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Winoo Home'),
-        ),
-        body: Center(
-          child: Text('Winoo is coming soon...'),
-        ),
-      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/home': (context) => const HomeScreen(),
+        '/admin': (context) => const AdminPanel(),
+        '/slot': (context) => const SlotMachineScreen(),
+        '/dice': (context) => const DiceGameScreen(),
+        '/wheel': (context) => const WheelSpinScreen(),
+      },
     );
   }
 }
