01c84f6dce0027dc3ddd27605392c48ff236f92b
hassanluice <hassanluiceofficial@gmail.com>
2025-04-09 23:02:05 +0600

    Create admin_panel.dart

---
+++ lib/lib/screens/lib/screens/lib/screens/admin_panel.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdminPanel extends StatelessWidget {
+  const AdminPanel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Admin Panel')),
+      body: const Center(
+        child: Text('Welcome Admin! All controls will be here.'),
+      ),
+    );
+  }
+}
